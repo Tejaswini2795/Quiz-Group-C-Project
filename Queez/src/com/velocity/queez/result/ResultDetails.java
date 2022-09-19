d4f27ac56b8b0f178f9518554e0cd1cba9891246
Aakash Kulkarni <113126215+ArKulkarni99@users.noreply.github.com>
2022-09-19 22:23:04 +0530

    Delete ResultDetails.java

--- Queez/src/com/velocity/queez/result/ResultDetails.java
+++
@@ -1,9 +0,0 @@
-package com.velocity.queez.result;
-
-import java.util.Map;
-
-public interface ResultDetails {
-	
-	public void getcalculatedResult(int studentId, Map<Integer, String> studentAnswers);
-
-}
